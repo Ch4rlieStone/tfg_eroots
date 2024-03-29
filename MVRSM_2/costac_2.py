Fix sign of Y reactive compensation

MVRSM_2/costac_2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import cmath
 import random
 
@@ -103,37 +104,42 @@ def costac_2(vol, n_cables, react1_bi, react2_bi, react3_bi, react4_bi, react5_b
 
     # 1.4 Compensator
     if react1_bi == 1:
-        Y_l1 = react1_val
+        Y_l1 = -1j * react1_val
     else:
         Y_l1 = 0
-        #react1_val = 0
 
     if react2_bi == 1:
-        Y_l2 = react2_val
+        Y_l2 = -1j * react2_val
         
     else:
         Y_l2 = 0
-        #react2_val = 0
 
     if react3_bi == 1:
-        Y_l3 = react3_val
+        Y_l3 = -1j * react3_val
     else:
         Y_l3 = 0
-        #react3_val = 0
 
     if react4_bi == 1:
-        Y_l4 = react4_val
+        Y_l4 = -1j * react4_val
     else:
         Y_l4 = 0
-        #react4_val = 0
+
     if react5_bi == 1:
-        Y_l5 = react5_val
+        Y_l5 = -1j * react5_val
     else:
         Y_l5 = 0
-        #react5_val = 0
 
     # 1.5 Grid connection
-    Y_g = (4.950 - 0.495j) / Y_ref # p.u.  Computed from SCR = 5 and Xg/Rg = 10
+    # Y_g = (4.950 - 0.495j) / Y_ref # p.u.  Computed from SCR = 5 and Xg/Rg = 10
+    scr = 5
+    xrr = 10
+    # z**2 = (xrr * r)**2 + r**2
+    # z**2 = (rr**2 * (xrr**2 + 1))
+    zgridm = V_ref**2 / (scr * p_owf * Sbase)
+    rgrid = np.sqrt(zgridm**2 / (xrr**2 + 1))
+    xgrid = xrr * rgrid
+    zgrid = rgrid + 1j * xgrid
+    Y_g = (1 / zgrid) / Y_ref
 
     Y_bus = np.array([[Y_trserie + Y_tr + Y_l1, -Y_trserie, 0, 0, 0, 0],
                   [-Y_trserie, Y_piserie + Y_pi + Y_l2 + Y_trserie, - Y_piserie, 0, 0, 0],
@@ -171,7 +177,7 @@ def costac_2(vol, n_cables, react1_bi, react2_bi, react3_bi, react4_bi, react5_b
     Q_obj = np.array([q_owf, 0, 0, 0, 0])
     PQ_obj = np.concatenate([P_obj, Q_obj])
 
-    max_iter = 100
+    max_iter = 20
 
     while epsilon > 1e-6 and k < max_iter:
 
@@ -275,167 +281,152 @@ def costac_2(vol, n_cables, react1_bi, react2_bi, react3_bi, react4_bi, react5_b
 
     if k == max_iter:
         print("NO SOLUTION !!!")
-
-    #  Now we can get the P ad Q values of the slack node 6 (note that the slack node is the last one of the vectors)
-
-    p_wslack = np.zeros(nbus)
-    for i in range(nbus):
-            for j in range(nbus):
-                p_wslack[i] = p_wslack[i] + V_wslack[i] * V_wslack[j] * (np.real(Y_bus[i, j]) * np.cos(
-                    angle_wslack[i]-angle_wslack[j]) + np.imag(Y_bus[i, j]) * np.sin(angle_wslack[i]-angle_wslack[j]))
-                
-    #print("p_final =" , p_wslack)
-
-    q_wslack = np.zeros(nbus)
-    for i in range(nbus):
-            for j in range(nbus):
-                q_wslack[i] = q_wslack[i] + V_wslack[i] * V_wslack[j] * (np.real(Y_bus[i, j]) * np.sin(
-                    angle_wslack[i]-angle_wslack[j]) - np.imag(Y_bus[i, j]) * np.cos(angle_wslack[i]-angle_wslack[j]))
-                
-    #print("q_final =" , q_wslack)
-
-    # Now we compute the injected currents at each node
-
-    curr_inj = np.zeros(nbus, dtype = 'cfloat')
-    for i in range(nbus):
-        for j in range(nbus):
-            curr_inj[i] = curr_inj[i] + Y_bus[i,j] * cmath.rect(V_wslack[j],angle_wslack[j])
-
-    #print("currents injections =", curr_inj)
-
-
-    #  When it comes to overcurrents, we are interested in line currents, not node injection currents.
-    #  We compute now line currents. Note they are normalized to the power of the plant
-
-    i_21 = abs((cmath.rect(V[0],angles[0]) - cmath.rect(V[1],angles[1])) * Y_trserie) / p_owf
-    i_32 = abs((cmath.rect(V[1],angles[1]) - cmath.rect(V[2],angles[2])) * Y_piserie) / p_owf
-    i_43 = abs((cmath.rect(V[2],angles[2]) - cmath.rect(V[3],angles[3])) * Y_piserie) / p_owf
-    i_54 = abs((cmath.rect(V[3],angles[3]) - cmath.rect(V[4],angles[4])) * Y_trserie) / p_owf
-
-    curr = np.array([i_21, i_32, i_43, i_54])
-    #print("currents =", curr)
-
-    #  i_g5 = abs((V_wslack[5] - V[4]) * Y_trserie)
-
-    
-    
-
-
-    #  We compute the AC power losses
-
-    p_lossac = Sbase * (p_owf + p_wslack[5]) * 1e-6  # MW
-    # print("ac_losses =", p_lossac)
-
-    #print ("ac_losses =", p_lossac,"MW")
-
-    #  NOW WE CREATE THE COST FUNCTION THAT WILL ALLOWAS TO IMPLEMENT THE MVRSM ALGORITHM
-    order_mg = 1
-    #  Cable cost
-   
-    c_cab = (((A + B * np.exp(C * (n_cables * np.sqrt(3) * u_i * I_rated) * 1e-6) + D) * (9 * n_cables + 1) * l) / (10 * E)) * 1e-6  #  to get  A,B,C,D and I max parameters we will use
-                                                                                            #  manufacturer data (110,150,220) S_rcb in MVA
-
-    #  Cost switchgears
-    c_gis = (0.0017 * u_i * 1e-3 + 0.0231)  # u_i in kV
-
-    # Cost susbstation
-    c_ss = (2.534 + 0.0887 * p_owf * 100)  # p-owf in MW
-
-    # Cost power losses
-    t_owf = 25  # lie time in years
-    c_ey = 100  # eu/MWh, cost of energy lost 
-    c_losses = (8760 * t_owf * c_ey * p_lossac) / 1e6 # losses in MW , 8760 since 1 year is 8760 h
-
-    # Cost transformers
-    c_tr = (0.0427 * (S_rtr * 1e-6)**0.7513)  # S_rtr in MVA
-
-    # Cost reactors
-    k_on = 0.01049
-    k_mid = 0.01576
-    k_off = 0.01576
-    p_on = 0.8312
-    p_mid = 1.244
-    p_off = 1.244
-
-    if react1_bi == 1:
-        c_r1 = k_off * (abs(Y_l1) * (1)**2) + p_off
-    else:
-        c_r1 = 0
-
-    if react2_bi == 1:
-        c_r2 = k_off * (abs(Y_l2) * (1)**2) + p_off
-    else:
-        c_r2 = 0
-
-    if react3_bi == 1:
-        c_r3 = k_mid * (abs(Y_l3) * (1)**2) + p_mid
-    else:
-        c_r3 = 0
-
-    if react4_bi == 1:
-        c_r4 = k_on * (abs(Y_l4) * (1)**2) + p_on
-    else:
-        c_r4 = 0
-
-    if react5_bi == 1:
-        c_r5 = k_on * (abs(Y_l5) * (1)**2) + p_on
-    else:
-        c_r5 = 0
-    
-    c_reac = c_r1 + c_r2 + c_r3 + c_r4 + c_r5
-    
-    #Total cost
-    # c_total = c_cab + c_gis + c_ss + c_tr + c_reac
-    c_total = c_cab + c_gis + c_tr + c_reac
-    #print("total cost =", c_total)
-
-    #  We have to include here the penalizations of voltages, current and reactive power
-    #  deviations since the are the inequality constraints. 
-
-    # over or below voltages
-    c_vol = 0
-    for i in range(nbus-1):
-        if V[i] > 1.1:
-            c_vol += (V[i] - 1.1) * 10000000
-        elif V[i] < 0.9:
-            c_vol += (0.9 - V[i]) * 10000000
-
-    #  print("overvoltage =", c_vol)
-
-    #overcurrents
-    c_curr = 0
-    for i in range(4):
-        if curr[i] > 1.1 * n_cables:
-            c_curr = c_curr + abs(curr[i] - 1.1 * n_cables) * 100000
-
-    #  print("overcurrent =", c_curr)
-
-    # we want reactive power delivered to the grid to be as close as possible to 0
-    c_react = 0
-    if q_wslack[nbus-1] != 0:
-            c_react = abs(q_wslack[nbus-1]) * 1000
-
-    #print("reactivetogrid =", c_react)
-
-    costs = np.array([c_total , c_vol, c_curr, c_react])
-    #print("costs =", costs)
-    total = costs.sum()
-    # print(c_total * 1e-8, c_vol + c_curr + c_react)
-    #cost_invest = c_total * 1e-7
-    cost_invest = c_total
-    # cost_tech = (c_vol + c_curr + c_react + c_losses) * 1
-    cost_tech = (c_vol + c_curr + c_losses) * 1
-    # cost_tech = c_losses
-
-    if c_losses < 100:
-        print(V)
-        print(p_lossac)
-        print('-------------')
-
-    if cost_tech > 1e6 or cost_tech < 1e-6:
-        cost_tech = 1e20
         cost_invest = 1e20
+        cost_tech = 1e20
 
+    else:
+        Iinj = Y_bus @ (V_wslack * np.exp(1j * angle_wslack))
+        Sinj = (V_wslack * np.exp(1j * angle_wslack)) * np.conj(Iinj)
+        #  Now we can get the P ad Q values of the slack node 6 (note that the slack node is the last one of the vectors)
+        p_wslack = np.zeros(nbus)
+        for i in range(nbus):
+                for j in range(nbus):
+                    p_wslack[i] = p_wslack[i] + V_wslack[i] * V_wslack[j] * (np.real(Y_bus[i, j]) * np.cos(
+                        angle_wslack[i]-angle_wslack[j]) + np.imag(Y_bus[i, j]) * np.sin(angle_wslack[i]-angle_wslack[j]))
+                    
+        #print("p_final =" , p_wslack)
+
+        q_wslack = np.zeros(nbus)
+        for i in range(nbus):
+                for j in range(nbus):
+                    q_wslack[i] = q_wslack[i] + V_wslack[i] * V_wslack[j] * (np.real(Y_bus[i, j]) * np.sin(
+                        angle_wslack[i]-angle_wslack[j]) - np.imag(Y_bus[i, j]) * np.cos(angle_wslack[i]-angle_wslack[j]))
+                    
+        #print("q_final =" , q_wslack)
+
+        # Now we compute the injected currents at each node
+
+        curr_inj = np.zeros(nbus, dtype = 'cfloat')
+        for i in range(nbus):
+            for j in range(nbus):
+                curr_inj[i] = curr_inj[i] + Y_bus[i,j] * cmath.rect(V_wslack[j],angle_wslack[j])
+
+        #print("currents injections =", curr_inj)
+
+
+        #  When it comes to overcurrents, we are interested in line currents, not node injection currents.
+        #  We compute now line currents. Note they are normalized to the power of the plant
+
+        i_21 = abs((cmath.rect(V[0],angles[0]) - cmath.rect(V[1],angles[1])) * Y_trserie) / p_owf
+        i_32 = abs((cmath.rect(V[1],angles[1]) - cmath.rect(V[2],angles[2])) * Y_piserie) / p_owf
+        i_43 = abs((cmath.rect(V[2],angles[2]) - cmath.rect(V[3],angles[3])) * Y_piserie) / p_owf
+        i_54 = abs((cmath.rect(V[3],angles[3]) - cmath.rect(V[4],angles[4])) * Y_trserie) / p_owf
+
+        curr = np.array([i_21, i_32, i_43, i_54])
+        #print("currents =", curr)
+
+        #  i_g5 = abs((V_wslack[5] - V[4]) * Y_trserie)
+
+        
+        
+
+
+        #  We compute the AC power losses
+
+        p_lossac = Sbase * (p_owf + p_wslack[5]) * 1e-6  # MW
+        # print("ac_losses =", p_lossac)
+
+        #print ("ac_losses =", p_lossac,"MW")
+
+        #  NOW WE CREATE THE COST FUNCTION THAT WILL ALLOWAS TO IMPLEMENT THE MVRSM ALGORITHM
+        order_mg = 1
+        #  Cable cost
+    
+        c_cab = (((A + B * np.exp(C * (n_cables * np.sqrt(3) * u_i * I_rated) * 1e-6) + D) * (9 * n_cables + 1) * l) / (10 * E)) * 1e-6  #  to get  A,B,C,D and I max parameters we will use
+                                                                                                #  manufacturer data (110,150,220) S_rcb in MVA
+
+        #  Cost switchgears
+        c_gis = (0.0017 * u_i * 1e-3 + 0.0231)  # u_i in kV
+
+        # Cost susbstation
+        c_ss = (2.534 + 0.0887 * p_owf * 100)  # p-owf in MW
+
+        # Cost power losses
+        t_owf = 25  # lie time in years
+        c_ey = 100  # eu/MWh, cost of energy lost 
+        c_losses = (8760 * t_owf * c_ey * p_lossac) / 1e6 # losses in MW , 8760 since 1 year is 8760 h
+
+        # Cost transformers
+        c_tr = (0.0427 * (S_rtr * 1e-6)**0.7513)  # S_rtr in MVA
+
+        # Cost reactors
+        k_on = 0.01049
+        k_mid = 0.01576
+        k_off = 0.01576
+        p_on = 0.8312
+        p_mid = 1.244
+        p_off = 1.244
+
+        if react1_bi == 1:
+            c_r1 = k_off * (abs(Y_l1) * (V[0])**2) + p_off
+        else:
+            c_r1 = 0
+
+        if react2_bi == 1:
+            c_r2 = k_off * (abs(Y_l2) * (V[1])**2) + p_off
+        else:
+            c_r2 = 0
+
+        if react3_bi == 1:
+            c_r3 = k_mid * (abs(Y_l3) * (V[2])**2) + p_mid
+        else:
+            c_r3 = 0
+
+        if react4_bi == 1:
+            c_r4 = k_on * (abs(Y_l4) * (V[3])**2) + p_on
+        else:
+            c_r4 = 0
+
+        if react5_bi == 1:
+            c_r5 = k_on * (abs(Y_l5) * (V[4])**2) + p_on
+        else:
+            c_r5 = 0
+        
+        c_reac = c_r1 + c_r2 + c_r3 + c_r4 + c_r5
+        
+        #Total cost
+        # c_total = c_cab + c_gis + c_ss + c_tr + c_reac
+        c_total = c_cab + c_gis + c_tr + c_reac
+
+        #  We have to include here the penalizations of voltages, current and reactive power
+        #  deviations since the are the inequality constraints. 
+
+        # over or below voltages
+        c_vol = 0
+        for i in range(nbus-1):
+            if V[i] > 1.1:
+                c_vol += (V[i] - 1.1) * 10000
+            elif V[i] < 0.9:
+                c_vol += (0.9 - V[i]) * 10000
+
+        #overcurrents
+        c_curr = 0
+        for i in range(4):
+            if curr[i] > 1.1 * n_cables:
+                c_curr = c_curr + abs(curr[i] - 1.1 * n_cables) * 10000
+
+        # we want reactive power delivered to the grid to be as close as possible to 0
+        c_react = 0
+        if q_wslack[nbus-1] != 0:
+                c_react = abs(q_wslack[nbus-1]) * 5000
+
+        cost_invest = c_total
+        cost_tech = (c_vol + c_curr + c_react + c_losses) * 1
+
+        cost_full = np.array([[c_vol, c_curr, c_losses, c_react, cost_tech, c_cab, c_gis, c_tr, c_reac, cost_invest]])
+        column_names = ['V', 'I', 'Pl', 'Q', 'Tech', 'Cab', 'Gis', 'Tr', 'Reac', 'Inv']
+        dfc = pd.DataFrame(cost_full, columns=column_names)
+        print(dfc)
     return np.array([cost_invest, cost_tech])
 
 

MVRSM_2/costs_debug.py
@@ -2,7 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def c_cabs(n_cables):
+def c_cabs1(n_cables):
+    # ref 12 
+    # Cable costs
+    # :param n_cables: number of cables
+    # return: cost in M€
+    ll = 100  # km
+    u_i = 110e3
+    R = 0.0067  # ohm/km
+    Cap = 0.24e-6  # F/km
+    L = 0.36e-3   # H/km
+    A = 1.3295
+    B = 0.417
+    C = 0.01855
+    D = 17e4
+    E = 8.98
+    I_rated= 470
+    return (((A + B * np.exp(C * (np.sqrt(3) * u_i * I_rated) * 1e-6) + D) * (9 * n_cables + 1) * ll) / (10 * E)) / 1e6
+
+def c_cabs2(n_cables):
     # ref 12 
     # Cable costs
     # :param n_cables: number of cables
@@ -18,8 +36,27 @@ def c_cabs(n_cables):
     D = 17e4
     E = 8.98
     I_rated = 500
-    # return (((A + B * np.exp(C * (n_cables * np.sqrt(3) * u_i * I_rated) * 1e-6) + D) * (9 * n_cables + 1) * l) / (10 * E))
     return (((A + B * np.exp(C * (np.sqrt(3) * u_i * I_rated) * 1e-6) + D) * (9 * n_cables + 1) * ll) / (10 * E)) / 1e6
+
+def c_cabs3(n_cables):
+    # ref 12 
+    # Cable costs
+    # :param n_cables: number of cables
+    # return: cost in M€
+    ll = 100  # km
+    u_i = 220e3
+    R = 0.0067  # ohm/km
+    Cap = 0.17e-6  # F/km
+    L = 0.40e-3   # H/km
+    A = 3.181
+    B = 0.11
+    C = 0.0116
+    D = 17e4
+    E = 8.98
+    I_rated = 540  # how we get this value? 
+    return (((A + B * np.exp(C * (np.sqrt(3) * u_i * I_rated) * 1e-6) + D) * (9 * n_cables + 1) * ll) / (10 * E)) / 1e6
+
+
 
 def c_sw(v_nom):
     # ref 15
@@ -58,21 +95,25 @@ def c_loss(P_loss):
     return (8760 * 25 * 100 * P_loss) / 1e6
 
 if __name__ == "__main__":
-    n_cab = 2
+    n_cab = 3
     v_nom = 220
     s_tra = 750
     q_ac = 80
     p_ow = 500
     p_loss = 2
 
-    c_cab = c_cabs(n_cab)
+    c_cab1 = c_cabs1(n_cab)
+    c_cab2 = c_cabs2(n_cab)
+    c_cab3 = c_cabs3(n_cab)
     c_swi = c_sw(v_nom)
     c_tra = c_tr(s_tra)
     c_qac = c_qq(q_ac)
     c_sub = c_ss(p_ow)
     c_los = c_loss(p_loss)
 
-    print(f"Cost of cables: {c_cab} M€", sep="\n")
+    print(f"Cost of cables: {c_cab1} M€", sep="\n")
+    print(f"Cost of cables: {c_cab2} M€", sep="\n")
+    print(f"Cost of cables: {c_cab3} M€", sep="\n")
     print(f"Cost of switchgear: {c_swi} M€", sep="\n")
     print(f"Cost of transformer: {c_tra} M€", sep="\n")
     print(f"Cost of reactive power compensation: {c_qac} M€", sep="\n")

MVRSM_2/main2.py
@@ -10,10 +10,11 @@ if __name__ == '__main__':
 
     ff = costac_2.costac_2
     d = 13 # Total number of variables
-    # lb = np.array([1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 450e6])  # Lower bound
+    lb = np.array([1, 1, 0, 0, 0, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 800e6])  # Lower bound
+    ub = np.array([3, 3, 0, 0, 0, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 800e6])  # Upper bound
+    # ub = np.array([3, 3, 1, 1, 1, 1, 1, 0.5, 0.5, 0.5, 0.5, 0.5, 800e6])  # Upper bound
+    # lb = np.array([1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 450e6])  # Lower bound
     # ub = np.array([3, 3, 1, 1, 1, 1, 1, 0.8, 0.8, 0.8, 0.8, 0.8, 750e6])  # Upper bound
-    lb = np.array([1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 450e6])  # Lower bound
-    ub = np.array([3, 3, 1, 1, 1, 1, 1, 0.8, 0.8, 0.8, 0.8, 0.8, 750e6])  # Upper bound
 
     num_int = 7 # number of integer variables
 	
@@ -22,7 +23,7 @@ if __name__ == '__main__':
     x0[num_int:d] = np.random.rand(d-num_int)*(ub[num_int:d]-lb[num_int:d]) + lb[num_int:d] # Random initial guess (continuous)
 	
     rand_evals = 200 # Number of random iterations
-    n_itrs = 200
+    n_itrs = 500
     n_trials = 1
     max_evals = n_itrs+rand_evals # Maximum number of MVRSM iterations, the first <rand_evals> are random
 	
@@ -75,16 +76,18 @@ if __name__ == '__main__':
     # plt.show()
     cmap = plt.get_cmap('viridis', max_evals)
     for i in range(max_evals):
-    #    plt.subplot(1,2,2)
+    #     plt.subplot(1,2,2)
         plt.scatter(yp[i, 0], yp[i, 1], color=cmap(i))
-    #for i in range(len(ys)):
-        #plt.subplot(1,2,1)
-        #plt.scatter(ys[i, 0], ys[i, 1], color=cmap(i))
+    # for i in range(len(ys)):
+    #     plt.subplot(1,2,1)
+        # plt.scatter(ys[i, 0], ys[i, 1], color=cmap(i))
         
 
     sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin=0, vmax=max_evals-1))
     plt.colorbar(sm, label='Point index')
-    plt.ylim(0, 5e3)
-    plt.xlim(5, 20)
+    plt.ylim(0, 20e3)
+    plt.xlim(0, 20)
     # plt.scatter(yp[:,0], yp[:,1])
     plt.show()
+    print(xs)
+    print(ys)