ed4aa078b2bdb3c180567dc3ff8ee17ac5796348
JosepFanals <jfanals13@gmail.com>
2024-04-01 14:13:24 -0700

    Tried a bit different bounds

--- MVRSM_2/main2.py
+++ MVRSM_2/main2.py
@@ -12,6 +12,8 @@ if __name__ == '__main__':
     d = 13 # Total number of variables
     lb = np.array([1, 1, 0, 0, 0, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 500e6])  # Lower bound
     ub = np.array([3, 3, 1, 1, 1, 1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 800e6])  # Upper bound
+    # lb = np.array([3, 3, 0, 0, 0, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 500e6])  # Lower bound
+    # ub = np.array([3, 3, 0, 0, 0, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 800e6])  # Upper bound
 
     num_int = 7 # number of integer variables
     x0 = np.zeros(d) # Initial guess
